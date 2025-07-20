d4ba76b8e23433da7f56ec50a21d4039f59fa0dd
FrankeFelipe5 <frankefelipee@gmail.com>
2025-07-20 18:49:45 -0300

    refactor: remove unused import in DriverLoader interface

--- src/main/java/dev/franke/felipe/job_applications/database/loader/DriverLoader.java
+++ src/main/java/dev/franke/felipe/job_applications/database/loader/DriverLoader.java
@@ -1,9 +1,6 @@
 package dev.franke.felipe.job_applications.database.loader;
 
-import dev.franke.felipe.job_applications.database.exception.ConnectionException;
 import dev.franke.felipe.job_applications.database.exception.LoadDriverException;
-
-import java.sql.Connection;
 
 public interface DriverLoader {
     void load() throws LoadDriverException;
